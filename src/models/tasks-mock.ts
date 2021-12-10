7815a632ebeb090d8ff5a88eb622e86aeb1cc48c
ku <ku@kode.ru>
2021-12-10 13:59:37 +0200

    feat(readme): add mock, update todo

---
+++ src/models/tasks-mock.ts
@@ -0,0 +1,67 @@
+import { Task } from "./types";
+
+export const tasksMockData: Record<string, Task> = {
+  "1": {
+    id: "1",
+    description: "Some 1",
+    status: "progress",
+    userId: "1",
+    title: "Kevin some task",
+  },
+  "2": {
+    id: "2",
+    description: "Some 2",
+    status: "new",
+    userId: "1",
+    title: "Kevin some task",
+  },
+  "3": {
+    id: "3",
+    description: "Some 3",
+    status: "done",
+    userId: "2",
+    title: "Kevin some task",
+  },
+  "4": {
+    id: "4",
+    description: "Some 4",
+    status: "progress",
+    userId: "2",
+    title: "Kevin some task",
+  },
+  "5": {
+    id: "5",
+    description: "Some 5",
+    status: "new",
+    userId: "2",
+    title: "Kevin some task",
+  },
+  "6": {
+    id: "6",
+    description: "Some 6",
+    status: "new",
+    userId: "3",
+    title: "Kevin some task",
+  },
+  "7": {
+    id: "7",
+    description: "Some 7",
+    status: "new",
+    userId: "3",
+    title: "Kevin some task",
+  },
+  "8": {
+    id: "8",
+    description: "Some 8",
+    status: "progress",
+    userId: "3",
+    title: "Kevin some task",
+  },
+  "9": {
+    id: "9",
+    description: "Some 9",
+    status: "progress",
+    userId: "3",
+    title: "Kevin some task",
+  },
+};
